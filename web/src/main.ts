9d0659d2e9c53e694e1961e4096e4b74c7e53dc0
Kiana Kaslana <16158440+qiyana423@user.noreply.gitee.com>
2025-08-27 20:02:22 +0800

    fix:优化前端显示

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,6 +5,9 @@ import App from './App.vue'
 import router from './router'
 import {pinia} from "@/stores";
 
+// 导入 Element Plus 样式
+import 'element-plus/dist/index.css'
+
 // 全局导入md-editor-v3样式，避免重复导入导致的样式冲突
 import 'md-editor-v3/lib/style.css'
 import 'md-editor-v3/lib/preview.css'
