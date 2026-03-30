2d593f1888641e576f46f79debb91e14b7dd0312
Kiana Kaslana <qiyana775@gmail.com>
2026-03-30 13:52:18 +0800

    del: 清除无效文件夹

--- server-blog/internal/model/response/friend_link.go
+++
@@ -1,8 +0,0 @@
-package response
-
-import "server/internal/model/database"
-
-type FriendLinkInfo struct {
-	List  []database.FriendLink `json:"list"`
-	Total int64                 `json:"total"`
-}
